1bdf86e454ff4444adabd40842a786a801febc2d
Arshia <arshia86jenab@gmail.com>
2026-02-14 01:08:49 +0330

    dasdasd

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./input.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <BrowserRouter basename="/valentine-card/">
+        <HashRouter basename="/valentine-card/">
             <App />
-        </BrowserRouter>
+        </HashRouter>
     </React.StrictMode>,
 );
